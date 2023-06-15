acc9cceda241911cfea2e34f2ab4cfe24942acd3
2023Ben <benroinvestments@gmail.com>
2023-06-15 16:30:05 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,6 @@
  * main  - orints the phrase
  * Programming is like building a multilingual puzzle, followed by a new line
  */
-
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
