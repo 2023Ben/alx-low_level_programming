fa103748ddaf2dc3b9460f5a948ee0623b334322
2023Ben <benroinvestments@gmail.com>
2023-06-15 16:51:03 -0700

    coding

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints put function followed nby a new line
+ * Return:0  success
+ */
+
+int main(void)
+{
+	puts("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
