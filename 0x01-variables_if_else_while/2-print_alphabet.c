9ffacc18adbdb868f4a2bb32fd07555316f181d3
2023Ben <benroinvestments@gmail.com>
2023-06-16 19:49:06 -0700

    programming

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,11 +7,13 @@
 
 int main(void)
 {
-	char ch;
+	char ch[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
 
-	for (ch = 'a';;ch++)
+	for (i = 0; i < 26; i++)
 	{
-		putchar(ch);
+		putchar(ch[i]);
+		putchar('\n');
 	}
 	return (0);
 }
