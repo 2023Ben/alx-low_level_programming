bb9ea0222c8ca3422dbca5433331b1173845da09
2023Ben <benroinvestments@gmail.com>
2023-06-20 14:06:45 -0700

    programming

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char c[] = "abcdefghijklmnopqrstuvwxyz";
+	int y;
+
+	for (y = 0; y <= 26 ; y++)
+	{
+		_putchar(c[y]);
+	}
+	_putchar('\n');
+}
