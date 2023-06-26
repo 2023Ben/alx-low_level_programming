8c5b7ea6b4b3d388f5f977b3ce9412ebb6103546
2023Ben <benroinvestments@gmail.com>
2023-06-26 14:23:56 -0700

    main.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+	int i, j, size;
+
+	for (i = 0; str[i] != 0; i++)
+	{
+		;
+	}
+
+	size = i;
+
+	for (j = size - 1; j >= 0; j--)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
