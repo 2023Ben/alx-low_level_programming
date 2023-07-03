75c41447030c8fa538cce886fc3a4442ee401e44
2023Ben <benroinvestments@gmail.com>
2023-07-03 10:29:56 -0700

    work with pointers

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _memset - A program
+*@s:pointer
+*@n:size of array
+*@b:char
+* Return:(s)
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int z = 0;
+
+while (z <= (n - 1))
+{
+s[z] = b;
+z++;
+}
+return (0);
+}
+
