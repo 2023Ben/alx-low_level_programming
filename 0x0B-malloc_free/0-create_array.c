38efb5c5c73ef65fc5a8e1f1da7fefd392fd83b8
2023Ben <benroinvestments@gmail.com>
2023-07-12 10:55:19 -0700

    0x0B. C - malloc, free

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars, and initializes it with
+ * a specific char
+ * @size: size of array
+ * @c: character to initialize with
+ *
+ * Return: Pointer
+ */
+char *create_array(unsigned int size, char c)
+{
+    unsigned int j;
+    char *s;
+
+    s = malloc(sizeof(char) * size);
+    if (s == 0)
+    return (0);
+    for ( j = 0; j <= size; j++)
+    {
+        *(s + j) = c;
+    }
+        *(s + j) = 0;
+    return (s);
+}
---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_grid - prints a grid of integers
+ * @grid: the address of the two dimensional grid
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: Nothing.
+ */
+int **alloc_grid(int width, int height)
+{
+        int **array;
+    array = malloc(width * sizeof(int *));
+    for(i = 0; i < width; i++)
+    {
+        array[i] = malloc(height * sizeof(int));
+    }
+    return array;
+}
