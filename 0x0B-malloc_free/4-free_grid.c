021727cd4f36a2e1ca329e52ee5ca968ad882e89
2023Ben <benroinvestments@gmail.com>
2023-07-12 15:02:02 -0700

    0x0B. C - malloc, free

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -6,13 +6,14 @@
 * @height: height of the grid
 * Return: Nothing.
 */
+
 void free_grid(int **grid, int height)
 {
 int i;
 
 for (i = 0; i < height; i++)
 {
-free(grid[i]);
+free(*(grid + i));
 }
-free grid;
+free(grid);
 }
