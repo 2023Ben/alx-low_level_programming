72ca7dfd3c2f35ba5b364b1c4f89ef0bb8bae521
2023Ben <benroinvestments@gmail.com>
2023-07-28 00:50:18 -0700

    0. Print list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* free_list - a function that frees a list_t list.
+* @head: struct
+* Return: free
+*/
+
+void free_list(list_t *head)
+{
+list_t *temp;
+
+while (head)
+{
+temp = head->next;
+free(head->str);
+free(head);
+head = temp;
+}
+}
+
