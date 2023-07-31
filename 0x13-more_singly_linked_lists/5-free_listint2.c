4c7546146f7ff2219ed6cf76c77ab2d8af4c01a8
2023Ben <benroinvestments@gmail.com>
2023-07-31 13:02:24 -0700

    Write a function that prints all the elements of a listint_t list.

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+* free_listint2 - Frees a listint_t list.
+*
+* @head: Pointer to the address of the head of the listint_t list.
+*
+*/
+void free_listint2(listint_t **head)
+{
+listint_t *temp;
+
+if (head == NULL)
+{
+return;
+}
+
+while (*head)
+{
+temp = (*head)->next;
+free(*head);
+*head = temp;
+}
+
+*head = NULL;
+}
