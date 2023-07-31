061dce80992e7037269bb91b889dc7dd8246b44a
2023Ben <benroinvestments@gmail.com>
2023-07-31 13:31:29 -0700

    Write a function that prints all the elements of a listint_t list.

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* pop_listint - deletes the head node of a listint_t list.
+* @head: Pointer to the address of the
+*        head of the listint_t list.
+*
+* Return: If the linked list is empty - 0 - The head node's data (n).
+*/
+int pop_listint(listint_t **head)
+{
+listint_t *temp;
+int num1;
+
+if (head == NULL)
+return (0);
+
+num1 = (*head)->n;
+temp = (*head)->next;
+free(*head);
+*head = temp;
+
+return (num1);
+}
