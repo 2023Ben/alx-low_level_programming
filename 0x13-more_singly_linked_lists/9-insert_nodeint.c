aaa0a2bab6d0ec6560d6d0df6a4785394c0c5db8
2023Ben <benroinvestments@gmail.com>
2023-08-01 04:47:16 -0700

    Write a function that prints all the elements of a listint_t list.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,49 +1,49 @@
 #include "lists.h"
-
-
-
 /**
- * insert_nodeint_at_index - insert node at the nth index of a linkedlist
- * @idx: index where the node should be added
- * @n: value of the node to be added
- * @head: the head of the list
- * Return: pointer to the head of the list
+ * insert_nodeint_at_index - inserts node at a particula index
+ * @head : pointer to the list
+ * @idx : position to insertt node
+ * Return : new node
  */
-
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *c, *node;
-
-	if (*head == NULL)
-		return (NULL);
-
-	node = malloc(sizeof(*node));
-
-	if (node == NULL)
+	unsigned int counter = 0;
+	listint_t *tmp;
+	listint_t *tmp1;
+	if (head == NULL)
 	{
-		free(node);
-		node = NULL;
 		return (NULL);
 	}
-
-	node->n = n;
-	node->next = NULL;
-	c = *head;
-
+	tmp = *head;
+	if (tmp == NULL)
+	{
+		return (NULL);
+	}
+	tmp1 = malloc(sizeof(listint_t));
+	if (tmp1 == NULL)
+	{
+		return (NULL);
+	}
+	tmp1->n = n;
 	if (idx == 0)
 	{
-		node->next = c;
-		*head = node;
-		return (*head);
+		tmp1->next = *head;
+		*head = tmp1;
+		return (tmp1);
 	}
-
-	while (--idx)
+	while (counter != idx - 1)
 	{
-		if (c == NULL || c->next == NULL)
+		tmp = tmp->next;
+		if (tmp == NULL)
+		{
+			free(tmp1);
 			return (NULL);
-		c = c->next;
+		}
+		counter++;
 	}
-	node->next = c->next;
-	c->next = node;
-	return (node);
+	tmp1->next = tmp->next;
+	tmp->next = tmp1;
+
+	return (tmp1);
+
 }
