22d042d0c626ce44599ddc194df2d9e0fbbeb662
2023Ben <benroinvestments@gmail.com>
2023-08-04 19:31:46 -0700

    bitwise done

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_endianness - Checks the endianness.
+ *
+ * Return: If big-endian - 0.
+ *         If little-endian - 1.
+ */
+int get_endianness(void)
+{
+int num = 1;
+char *ed = (char *)&num;
+
+if (*ed == 1)
+{
+return (1);
+}
+
+return (0);
+}
