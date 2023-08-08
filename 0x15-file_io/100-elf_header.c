e87d8d16edef02a52b03805f30ac873acbc1807e
2023Ben <benroinvestments@gmail.com>
2023-08-07 20:01:14 -0700

    file operation

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -177,7 +177,9 @@ void print_abi(unsigned char *e_ident)
 void print_type(unsigned int e_type, unsigned char *e_ident)
 {
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
+	{
 		e_type >>= 8;
+	}
 
 	printf("  Type:                              ");
 
